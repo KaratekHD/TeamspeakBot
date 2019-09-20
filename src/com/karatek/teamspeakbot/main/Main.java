797e5b403ae1c80e356e8b5b7dd35c5be69b9667
KaratekHD <jens@krieger-juhnke.de>
2019-09-20 17:23:25 +0200

    Added new features

--- src/com/karatek/teamspeakbot/main/Main.java
+++ src/com/karatek/teamspeakbot/main/Main.java
@@ -10,13 +10,18 @@ import com.github.theholywaffle.teamspeak3.TS3Config;
 import com.github.theholywaffle.teamspeak3.TS3Query;
 import com.github.theholywaffle.teamspeak3.api.TextMessageTargetMode;
 import com.github.theholywaffle.teamspeak3.api.event.*;
+import com.github.theholywaffle.teamspeak3.api.exception.TS3CommandFailedException;
+import com.github.theholywaffle.teamspeak3.api.exception.TS3ConnectionFailedException;
+import com.github.theholywaffle.teamspeak3.api.exception.TS3QueryShutDownException;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import com.github.theholywaffle.teamspeak3.api.wrapper.ClientInfo;
 import com.karatek.teamspeakbot.main.utils.prefixhelper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -35,11 +40,33 @@ public class Main {
 
 		System.out.println(prefixhelper.getPrefix() + "Connecting to " + ip + " with queryusername " + user + "...");
 		final TS3Query query = new TS3Query(config);
-		query.connect();
+		try {
+			query.connect();
+		} catch (TS3CommandFailedException e) {
+			System.err.println(prefixhelper.getPrefix() + "FATAL: TS3CommandFailed (Error 401)");
+			System.err.println(prefixhelper.getPrefix() + "Ending.");
+			System.exit(401);
+		} catch (TS3ConnectionFailedException e) {
+			System.out.println("FATAL: TS3ConnectionFailed (Error 304)");
+			System.err.println(prefixhelper.getPrefix() + "Ending.");
+			System.exit(304);
+		}
 		final TS3Api api = query.getApi();
+		if(!query.isConnected()) {
+			System.err.println(prefixhelper.getPrefix() + "FATAL: TS3ConnectionFailed (Error 304)");
+			System.err.println(prefixhelper.getPrefix() + "Ending.");
+			System.exit(304);
+		}
 		api.login(user, password);
 		api.selectVirtualServerByPort(12500);
-		api.setNickname("Karatek Teamspeak Bot");
+		try {
+			api.setNickname("Karatek Teamspeak Bot");
+		} catch (TS3CommandFailedException e) {
+			System.err.println(prefixhelper.getPrefix() + "FATAL: TS3CommandFailed (Error 401)");
+			System.err.println(prefixhelper.getPrefix() + "Ending.");
+			System.exit(401);
+		}
+
 		System.out.println(prefixhelper.getPrefix() + "Connected successfully!");
 		api.sendServerMessage("I am online!");
 		api.registerAllEvents();
@@ -62,8 +89,13 @@ public class Main {
 								break;
 							case "!exit":
 								api.sendServerMessage("I am offline!");
-								try{ api.setNickname("NameHere"); }catch (Exception ignored){ }
-								query.exit();
+								try{ api.setNickname("KTS_Internal"); }catch (Exception ignored){ }
+								try {
+									query.exit();
+								} catch (TS3QueryShutDownException ex) {
+									System.err.println(prefixhelper.getPrefix() + "FATAL: TS3QuerySHutdownException (Error 402)");
+									System.exit(402);
+								}
 								System.out.print((char)13);
 								System.out.println(prefixhelper.getPrefix() + "Shutting down.");
 								System.exit(0);
@@ -75,7 +107,7 @@ public class Main {
 				} else {
 					if(e.getTargetMode() != TextMessageTargetMode.CLIENT && clientId != e.getInvokerId()) {
 						System.out.print((char)13);
-						System.out.println(prefixhelper.getPrefix() + api.getClientInfo(e.getInvokerId()).getNickname() + " said: " + e.getMessage());
+						System.out.println(prefixhelper.getPrefix() + api.getClientInfo(e.getInvokerId()).getNickname() + "[" + e.getInvokerId() + "]" + " said: " + e.getMessage());
 						System.out.print("> ");
 					}
 				}
@@ -98,12 +130,13 @@ public class Main {
 
 			@Override
 			public void onServerEdit(ServerEditedEvent serverEditedEvent) {
-
+				System.out.println("\b\b" + prefixhelper.getPrefix() + "The Server was edited by " + serverEditedEvent.getInvokerName());
+				log("The Server was edited by " + serverEditedEvent.getInvokerName() + " [" + serverEditedEvent.getInvokerId() + "]");
 			}
 
 			@Override
 			public void onChannelEdit(ChannelEditedEvent channelEditedEvent) {
-
+				log("The channel " + api.getChannelInfo(channelEditedEvent.getChannelId()).getName() + " was edited by " + channelEditedEvent.getInvokerName() + " [" + channelEditedEvent.getInvokerId() + "]");
 			}
 
 			@Override
@@ -135,31 +168,40 @@ public class Main {
 
 			@Override
 			public void onChannelCreate(ChannelCreateEvent channelCreateEvent) {
-
+				log("Channel " + api.getChannelInfo(channelCreateEvent.getChannelId()).getName() + " was created by " + channelCreateEvent.getInvokerName() + " [" + channelCreateEvent.getInvokerId() + "]");
 			}
 
 			@Override
 			public void onChannelDeleted(ChannelDeletedEvent channelDeletedEvent) {
-
+				log("Channel " + api.getChannelInfo(channelDeletedEvent.getChannelId()).getName() + " was deleted by " + channelDeletedEvent.getInvokerName() + " [" + channelDeletedEvent.getInvokerId() + "]");
 			}
 
 			@Override
 			public void onChannelMoved(ChannelMovedEvent channelMovedEvent) {
-
+				log("Channel " + api.getChannelInfo(channelMovedEvent.getChannelId()).getName() + " was moved by " + channelMovedEvent.getInvokerName());
 			}
 
 			@Override
 			public void onChannelPasswordChanged(ChannelPasswordChangedEvent channelPasswordChangedEvent) {
-
+				log("The password of channel " + api.getChannelInfo(channelPasswordChangedEvent.getChannelId()).getName() + " was changed by " + channelPasswordChangedEvent.getInvokerName());
 			}
 
 			@Override
 			public void onPrivilegeKeyUsed(PrivilegeKeyUsedEvent privilegeKeyUsedEvent) {
+				log("Client " + api.getClientInfo(privilegeKeyUsedEvent.getClientId()).getNickname() + " used a privilege key :" + privilegeKeyUsedEvent.getPrivilegeKey());
+				try {
+					api.sendPrivateMessage(privilegeKeyUsedEvent.getClientId(), "Your server group was added. The privilege key " + privilegeKeyUsedEvent.getPrivilegeKey() + " is now used." );
+				} catch (TS3CommandFailedException e) {
+					log("TS3CommandFailed (Error 401)");
+				}
+
 
 			}
 		});
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input;
+		int selected_id;
+		ClientInfo selected = null;
 		while(true) {
 			System.out.print("> ");
 			input = br.readLine();
@@ -169,8 +211,15 @@ public class Main {
 				case "exit":
 				case "stop":
 					api.sendChannelMessage("I am offline!");
-					try{ api.setNickname("NameHere"); }catch (Exception ignored){ }
-					query.exit();
+					try{ api.setNickname("KTS_Internal"); }catch (Exception e){
+						System.out.println(prefixhelper.getPrefix() + "Internal Error. (Error 300)");
+					}
+					try {
+						query.exit();
+					} catch (TS3QueryShutDownException ex) {
+						System.err.println(prefixhelper.getPrefix() + "FATAL: TS3QuerySHutdownException (Error 402)");
+						System.exit(402);
+					}
 					System.out.print((char)13);
 					System.out.println(com.karatek.teamspeakbot.main.utils.prefixhelper.getPrefix() + "Shutting down.");
 					System.exit(0);
@@ -191,8 +240,80 @@ public class Main {
 					if(strings.length != 2) {
 						System.out.println("Usage: select <id>");
 					} else {
-						System.out.println("Well.");
+						if(isInteger(strings[1])) {
+							selected_id = Integer.parseInt(strings[1]);
+							boolean worked = true;
+							try {
+								selected = api.getClientInfo(selected_id);
+							} catch (TS3CommandFailedException e) {
+								worked = false;
+								System.err.println(prefixhelper.getPrefix() + "TS3CommandFailedException, maybe the client is not online? (Error 401)");
+							}
+							if (worked) {
+								System.out.println("You selected " + selected.getNickname() + ".");
+							}
+						} else {
+							System.out.println("Usage: select <id>");
+						}
 					}
+					break;
+				case "whoami":
+					System.out.println(clientId);
+					break;
+				case "info":
+					if(selected == null) {
+						System.out.println("Please make a selection.");
+					} else {
+						selected = api.getClientInfo(selected.getId());
+						String output_info = "General information\n" +
+								"Nickname: " + selected.getNickname() + "\n" +
+								"Description: " + selected.getDescription() + "\n" +
+								"Location: " + selected.getCountry() + "\n" +
+								"Platform: " + selected.getPlatform() + "\n\n" +
+								"Channel information\n" +
+								"Channel name: " + api.getChannelInfo(selected.getChannelId()).getName() + "\n" +
+								"Channel description: " + api.getChannelInfo(selected.getChannelId()).getDescription() + "\n\n" +
+								"Internal information\n\n" +
+								"ID: " + selected.getId() + "\n" +
+								"UUID: " + selected.getUniqueIdentifier() + "\n" +
+								"Database ID: " + selected.getDatabaseId() + "\n" +
+								"IP: " + selected.getIp()+ "\n";
+
+						System.out.println(output_info);
+					}
+					break;
+				case "kick":
+					if(selected == null) {
+						System.out.println("Please make a selection.");
+					} else {
+						String message_kick = "";
+						try {
+
+							int kick_counted = 1;
+							while (kick_counted < strings.length) {
+								message_kick = message_kick + " " + strings[kick_counted];
+								kick_counted++;
+							}
+							if(message_kick.startsWith(" ")) {
+								message_kick = message_kick.substring(1);
+							}
+							api.kickClientFromServer(message_kick, selected);
+						} catch (TS3CommandFailedException e) {
+							System.out.println("Internal Error. (Error Code 401)");
+						}
+						if (message_kick != null) {
+							System.out.println("\b\b" + prefixhelper.getPrefix() + "Client " + selected.getNickname() + " was kicked");
+						} else {
+							System.out.println("\b\b" + prefixhelper.getPrefix() + "Client " + selected.getNickname() + " was kicked for " + message_kick);
+						}
+						try {
+							TimeUnit.MILLISECONDS.sleep(10);
+						} catch (InterruptedException e) {
+							System.out.println(prefixhelper.getPrefix() + "Interrupted. (Error 302)");
+						}
+
+					}
+					break;
                 case "list":
 				case "ls":
                     int list_counted = 0;
@@ -223,6 +344,9 @@ public class Main {
 							"'exit' - End this bot\n" +
 							"'say <text>' - Send a message to the server.\n" +
 							"'ls' - list all clients\n" +
+							"'select <id> - select a client\n" +
+							"'info' - get information about a client, use 'select' first!\n" +
+							"'kick' (<message>) - kicks a client with an optional message, use 'select' first!\n" +
 							"'help' - See this list");
 					break;
 			default:
@@ -232,4 +356,22 @@ public class Main {
 		}
 
 	}
+
+	public static boolean isInteger(String s) {
+    try {
+        Integer.parseInt(s);
+    } catch(NumberFormatException e) {
+        return false;
+    } catch(NullPointerException e) {
+        return false;
+    }
+    // only got here if we didn't return false
+    return true;
+}
+
+	public static void log(String string) {
+		System.out.println("\b\b" + prefixhelper.getPrefix() + string);
+		System.out.print("> ");
+	}
+
 }
